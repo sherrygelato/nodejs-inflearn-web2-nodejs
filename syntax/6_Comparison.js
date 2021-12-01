02c836b430456db011572e580969c2de62d61296
sherrygelato <sherrygelato@kakao.com>
2021-12-01 14:20:57 +0900

    14. JavaScipts - Boolean.

--- syntax/6_Comparison.js
+++ syntax/6_Comparison.js
@@ -1,6 +1,6 @@
-console.log(1==1); //true
+console.log(1==1); //true => 좌항 우항 
 console.log(1==2); //false
 console.log(1>2); //false
 console.log(1<2); //false
-console.log(1===1); //true
+console.log(1===1); //true => 정확하게 비교
 console.log(1===2); //false
