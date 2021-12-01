b6cf3684e07eb8bb12c8a7fd4db68af25b064fcc
sherrygelato <sherrygelato@kakao.com>
2021-12-01 14:40:30 +0900

    18. Node.js - 콘솔에서의 입력값

--- syntax/8_conditinal.js
+++ syntax/8_conditinal.js
@@ -1,6 +1,19 @@
+var args = process.argv;
+console.log(args);
+/*
+실행: 
+E:\practice\nodejs\syntax>node 8_conditinal.js sherry
+output:
+[
+  'C:\\Program Files\\nodejs\\node.exe', // nodejs 런타임이 어디에 있는지
+  'E:\\practice\\nodejs\\syntax\\8_conditinal.js', // 실행시킨 파일 경로 
+  'sherry'
+]
+*/
+console.log(args[2]);
 console.log('A');
 console.log('B');
-if(false){
+if(args[2] === '1'){
   console.log('C1');
 } else {
   console.log('C2');
